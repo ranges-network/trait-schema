a6d8b53e290a8140b3ae4b4d0247347969e74999
Bill Duncan <wdduncan@users.noreply.github.com>
2025-08-13 17:47:44 -0400

    add test ttl data

--- src/test-linkml/build-data.sh
+++ src/test-linkml/build-data.sh
@@ -1,2 +1,2 @@
 ../scripts/spreadsheet-to-rdf.py -i ASUoccurrence-reduced-100.csv -s traitvalue-test.yml > data.ttl
-robot merge -i traitvalue.ttl -i data.ttl -o ranges-data.ttl
+robot merge -i traitvalue.ttl -i data.ttl -o ranges-test-data.ttl
