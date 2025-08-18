2eb955858916544d7ae8be54a331328869317a2b
Bill Duncan <wdduncan@users.noreply.github.com>
2025-08-18 17:49:39 -0400

    fix bug in loading punned properties into ranges data

--- src/test-linkml/build-owl.sh
+++ src/test-linkml/build-owl.sh
@@ -3,5 +3,5 @@ gen-owl ../schema/trait-value.yml > schema.tmp.ttl
 robot merge -I http://purl.obolibrary.org/obo/fovt.owl -i  schema.tmp.ttl -o traitvalue.tmp.ttl 
 
 # convert pun_as annotations to punning statements and merge with trait value ontology
-pun-annotations-to-ttl.py ../schema/trait-value.yml > pun.tmp.ttl
+../scripts/pun-annotations-to-ttl.py ../schema/trait-value.yml > pun.tmp.ttl
 robot merge -i traitvalue.tmp.ttl -i pun.tmp.ttl -o traitvalue.ttl
