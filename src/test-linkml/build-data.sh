3cc135e54f8b313464f5b3a97289d1b6ec1735b0
Bill Duncan <wdduncan@users.noreply.github.com>
2025-08-21 21:39:40 -0400

    remove mammary_count from schema

--- src/test-linkml/build-data.sh
+++ src/test-linkml/build-data.sh
@@ -1,2 +1,2 @@
-../scripts/spreadsheet-to-rdf.py -i ASUoccurrence-new-cols.csv -s traitvalue-test.yml > data.ttl
+../scripts/spreadsheet-to-rdf.py -i ASUoccurrence-new-cols.csv -s ../schema/trait-value.yml > data.ttl
 robot merge -i traitvalue.ttl -i data.ttl -o ranges-test-data.ttl
