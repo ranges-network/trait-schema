9ed618cc56c8d97de440c7721eec50a4165143ee
Bill Duncan <wdduncan@users.noreply.github.com>
2025-09-25 20:41:30 -0400

    update documentation about --include-prefixes flag

--- src/scripts/pun-annotations-to-ttl.py
+++ src/scripts/pun-annotations-to-ttl.py
@@ -54,22 +54,29 @@ def parse_prefixes(view:SchemaView) -> Text:
     return ttl_str
 
 
-def parse_element_uri(view:SchemaView, element:Definition) -> Text:
+def parse_element_uri(view:SchemaView, element:Definition, include_prefixes:bool) -> Text:
     """extract the URI from the schema element and inserts <> around the URI if necessary
 
     Args:
         view (SchemaView): schema view object
         element (Definition): schema element with the URI
+        include_prefixes (bool): 
+            flag that determines if prefixes created with output
+            if true: CURIEs are used identifiers
+            if false: full URIs are used as identifiers
 
     Returns:
         Text: element's URI
     """
-    uri = view.get_uri(element)
+    if include_prefixes:
+        uri = view.get_uri(element, expand=False)
+    else:
+        uri = view.get_uri(element, expand=True)
     
     # check for 'http' uri
     if uri[0:4] == 'http':
         uri = f"<{uri}>"
-    
+    # print(view.get_uri(element, expand=True))
     return uri
 
 
@@ -100,12 +107,16 @@ def parse_annotation(annotation:Annotation, uri:Text) -> Text:
     return ttl_str
 
 
-def parse_annotations(view:SchemaView, element:Definition) -> Text:
+def parse_annotations(view:SchemaView, element:Definition, include_prefixes:bool) -> Text:
     """iterates over annotations defined for an element and created turtle statements as needed
 
     Args:
         view (SchemaView): schema view object
         element (Definition): the element being examined for annotations
+        include_prefixes (bool): 
+            flag that determines if prefixes created with output
+            if true: CURIEs are used identifiers
+            if false: full URIs are used as identifiers
 
     Returns:
         Text: turtle statements based on annotation informaton
@@ -113,7 +124,7 @@ def parse_annotations(view:SchemaView, element:Definition) -> Text:
     # print('-----', type(element), type(element) == SlotDefinition)
     ttl_str = ""
     # return ttl_str
-    uri = parse_element_uri(view, element)
+    uri = parse_element_uri(view, element, include_prefixes)
 
     # parse annotatons
     for _, annotation in element.annotations.items():
@@ -124,11 +135,15 @@ def parse_annotations(view:SchemaView, element:Definition) -> Text:
     return ttl_str
 
 
-def parse_slot_annotations(view:SchemaView) -> Text:
+def parse_slot_annotations(view:SchemaView, include_prefixes:bool) -> Text:
     """Uses schema view to create turtle from the slot annotations.
 
     Args:
         view (SchemaView): SchemaView object
+        include_prefixes (bool): 
+            flag that determines if prefixes created with output
+            if true: CURIEs are used identifiers
+            if false: full URIs are used as identifiers
 
     Returns:
         Text: String containing turtle for the slots.
@@ -139,7 +154,7 @@ def parse_slot_annotations(view:SchemaView) -> Text:
     for slot in slot_keys:
         element = view.get_slot(slot)
         
-        ttl_str += parse_annotations(view, element)
+        ttl_str += parse_annotations(view, element, include_prefixes)
         if len(ttl_str) > 0:
             ttl_str += "\n" # add blank line for readabilty
 
@@ -147,19 +162,36 @@ def parse_slot_annotations(view:SchemaView) -> Text:
 
 
 def main(view:SchemaView, include_prefixes:bool):
+    """Uses schema view to create turtle statements.
+
+    Args:
+        view (SchemaView): SchemaView object
+        include_prefixes (bool): 
+            flag that determines if prefixes created with output
+            if true: CURIEs are used identifiers
+            if false: full URIs are used as identifiers
+
+    Returns:
+        None: Output is the result of printing turtle statements.
+    """
     if include_prefixes:
         prefixes_ttl = parse_prefixes(view)
         print(prefixes_ttl)
 
-    annotations_ttl = parse_slot_annotations(view)
+    annotations_ttl = parse_slot_annotations(view, include_prefixes)
     print(annotations_ttl)
 
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 @click.command(
         help="""
-        Reads the annotations in a LinkML schema and converts punning annotations into OWL.\n
+        Reads the annotations in a LinkML schema (with a .yml or yaml extenstion) and converts punning annotations into OWL.\n
         e.g., pun-annotations-to-ttl.py myschema.yaml
+
+        By default, the prefixes (e.g., "@prefix ex: <http://example.com#>") are output, and enities are identified using a CURIE (e.g., "ex:entity1").
+        This is needed inorder to merge the output with other ontology files.
+
+        To turn off this behavior, use the --no-include-prefixes flag, and entities are identified using the full URI (e.g., "<http://example.com#entity1>").
         """,
         context_settings=CONTEXT_SETTINGS)
 @click.argument ('schema')
